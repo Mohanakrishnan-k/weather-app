e2b4a095e165e100bb1b76b0eb467c44ece47227
Mohanakrishnan-k <mohanakrishnanking@gmail.com>
2025-12-19 06:43:32 -0800

    stoped .env tracking

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './Weather.css'
 import search from './assets/search.png'
 import Humidity from './assets/humidity.png'
